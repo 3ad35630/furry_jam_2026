2d45334788607b33b8e05e24fb241c152f2aa1d0
Silverson-EthanG <griffi38@wwu.edu>
2026-07-15 00:38:02 -0700

    Camera Map transition works smoothly!

--- ui/simple/scripts/moon_scene.gd
+++ ui/simple/scripts/moon_scene.gd
@@ -1,26 +1,29 @@
 extends Node3D
 
 @export var cameras : Array[Camera3D] = []
-@export var _dock : Location
-@export var _farm : Location
-@export var _woods : Location
+#@export var _dock : Location
+#@export var _farm : Location
+#@export var _woods : Location
 
 func return_to_world():
 	for i in range(cameras.size()):
-		cameras[i].clear_current(false)
-	cameras[0].make_current()
+		if cameras[i].current == true:
+			CameraTransition.transition_camera(cameras[i], cameras[0], 2.0)
 
 func go_to_docks():
-	cameras[0].clear_current(false)
-	cameras[2].make_current()
+	#cameras[0].clear_current(false)
+	#cameras[2].make_current()
+	CameraTransition.transition_camera(cameras[0], cameras[2], 2.0)
 
 func go_to_farm():
-	cameras[0].clear_current(false)
-	cameras[1].make_current();
+	#cameras[0].clear_current(false)
+	#cameras[1].make_current();
+	CameraTransition.transition_camera(cameras[0], cameras[1], 2.0)
 	
 func go_to_woods():
-	cameras[0].clear_current(false)
-	cameras[3].make_current()
+	#cameras[0].clear_current(false)
+	#cameras[3].make_current()
+	CameraTransition.transition_camera(cameras[0], cameras[3], 2.0)
 
 func _on_return_button_pressed() -> void:
 	$MainPanel/CanvasLayer2.visible = false
