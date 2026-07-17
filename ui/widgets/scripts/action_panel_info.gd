00bff64d282f011eb8e73339bfc7c36623bd0b4b
Fillan Swift <fillan.swift@gmail.com>
2026-07-17 14:53:49 -0700

    sloppy patch for sloppy code

--- ui/widgets/scripts/action_panel_info.gd
+++ ui/widgets/scripts/action_panel_info.gd
@@ -1,9 +1,9 @@
 extends Control
 class_name ActionPanelInfo
 
-@onready var name_label : Label = $NameLabel
-@onready var skill_label : Label = $SkillLabel
-@onready var description_label : Label = $DescriptionLabel
+@export var name_label : Label
+@export var skill_label : Label
+@export var description_label : Label
 
 func update(action : Action):
 	name_label.text = action.resource_name
