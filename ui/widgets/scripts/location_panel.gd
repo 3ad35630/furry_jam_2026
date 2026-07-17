31a61378102e70b295a336e6447562362468efe3
Fillan Swift <fillan.swift@gmail.com>
2026-07-16 23:59:33 -0700

    location info
    - added button to toggle location info visibility
    - changed clocks button to a toggle for clarity

--- ui/widgets/scripts/location_panel.gd
+++ ui/widgets/scripts/location_panel.gd
@@ -2,49 +2,64 @@ extends PanelContainer
 
 ## number of non-clocks at the start of the container
 
-@onready var button : Button = $MarginContainer/ButtonContainer/Button
-@onready var clockContainer : VBoxContainer = $MarginContainer/VBoxContainer/ClockList/ClockContainer
-@onready var clockList : VBoxContainer = $MarginContainer/VBoxContainer/ClockList
+@export var infoButton : Button
+@export var clockButton : Button
+@export var infoContainer : Control
+@export var clockContainer : Control
+@export var clockList : VBoxContainer
+@export var infoLabel : Label
 
 
 func _ready() -> void:
-	button.pressed.connect(_toggle_clocks)
+	infoButton.toggled.connect(_show_info)
+	clockButton.toggled.connect(_show_clocks)
 	Modes.gameplay_mode_changed.connect(_on_mode_changed)
 
 
-func _toggle_clocks() -> void:
-	clockList.visible = not clockList.visible
+func _show_info(state : bool = true) -> void:
+	infoContainer.visible = state
+	infoButton.button_pressed = state
+
+
+func _show_clocks(state : bool = true) -> void:
+	clockContainer.visible = state
+	clockButton.button_pressed = state
 
 
 func _on_mode_changed(mode : Enums.GameplayMode) -> void:
-	#clockList.hide()
+	_show_clocks(false)
+	_show_info(false)
 	
-	# get relevant clocks
+	# get relevant clocks and set location-dependent data
 	var clocks : Array[Clock] = []
 	match(mode):
 		Enums.GameplayMode.LOCATION:
+			infoLabel.text = Modes.current_location.description
+			infoButton.show()
 			clocks = Modes.current_location.accessible_clocks
 		Enums.GameplayMode.MAP:
+			infoLabel.text = ''
+			infoButton.hide()
 			# TODO: get all clocks in Map view
 			pass
 	
 	if clocks.is_empty():
-		clockList.hide()
-		button.disabled = true
+		#_show_clocks(false)
+		clockButton.disabled = true
 	else:
-		button.disabled = false
+		clockButton.disabled = false
 		
 		# add more widgets if necessary
-		var delta = clocks.size() - clockContainer.get_child_count()
+		var delta = clocks.size() - clockList.get_child_count()
 		if delta > 0:
-			var template = clockContainer.get_child(0)
+			var template = clockList.get_child(0)
 			for i in range(delta):
 				var newWidget = template.duplicate()
-				clockContainer.add_child(newWidget)
+				clockList.add_child(newWidget)
 	
 		# update widgets
-		for i in range(clockContainer.get_child_count()):
-			var widget = clockContainer.get_child(i)
+		for i in range(clockList.get_child_count()):
+			var widget = clockList.get_child(i)
 			if i < clocks.size():
 				widget.show()
 				widget.clock = clocks[i]
