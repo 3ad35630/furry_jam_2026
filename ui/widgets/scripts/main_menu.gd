ccec8a68d234187738550955f50130d07bcaee98
Phorzaeken <jeffduran000@Gmail.com>
2026-07-17 19:18:39 -0700

    Audio Update 003
    + Main Menu
    + Main Scene Configured too

--- ui/widgets/scripts/main_menu.gd
+++ ui/widgets/scripts/main_menu.gd
@@ -3,7 +3,8 @@ extends Panel
 
 func _ready() -> void:
 	Modes.gameplay_mode_changed.connect(_on_mode_changed)
-
+	$MuchAdoAboutTea.play()
 
 func _on_mode_changed(mode : Enums.GameplayMode) -> void:
 	visible = mode == Enums.GameplayMode.MAINMENU
+	$MuchAdoAboutTea.stop()
