b0dfe5316a55bc38b36df9bb325130bead253369
Zach Hall <59339962+3ad35630@users.noreply.github.com>
2026-07-19 14:37:04 -0400

    fixing several ui bugs with the dice
    - dice return to hand when you exit a location
    - dice continue to exist when you move them between actions
    - dice that fail to drag from the inventory do not disappear

--- ui/simple/scripts/character_dice.gd
+++ ui/simple/scripts/character_dice.gd
@@ -1,28 +1,28 @@
 extends HBoxContainer
 
-var hbox_dict : Dictionary[Character, HBoxContainer] = {}
 @export var char_dice : PackedScene
 @export var witch : Resource
 
 func _ready() -> void:
+	Modes.gameplay_mode_changed.connect(on_gameplay_mode_changed)
 	for character in Characters.characters:
 		if character == witch:
+			witch = character # we want them to be the same Resource
 			character.dice_updated.connect(update_dice_display_for_character)
-			var new_hbox = HBoxContainer.new()
-			add_child(new_hbox)
-			hbox_dict[character] = new_hbox
 			update_dice_display_for_character(character)
 		
 func update_dice_display_for_character(character : Character) -> void:
-	for child in hbox_dict[character].get_children():
-		hbox_dict[character].remove_child(child)
+	for child in get_children():
 		child.queue_free()
 	for die in character.dice_dicts:
 		var new_dice_display = char_dice.instantiate()
 		new_dice_display.data = die
-		hbox_dict[character].add_child(new_dice_display)
-
-func on_skill_die_removed(data):
-	for display in hbox_dict[data['character']].get_children():
-		if display.data['index'] == data['index']:
-			display.visible = true
+		new_dice_display.remove_if_successfully_dragged = true
+		add_child(new_dice_display)
+		
+func on_gameplay_mode_changed(new_mode : Enums.GameplayMode) -> void:
+	match new_mode:
+		Enums.GameplayMode.LOCATION:
+			update_dice_display_for_character(witch)
+		Enums.GameplayMode.MAP:
+			update_dice_display_for_character(witch)
--- ui/simple/scripts/draggable_die_display.gd
+++ ui/simple/scripts/draggable_die_display.gd
@@ -1,6 +1,11 @@
 extends DieDisplay
 		
 @export var draggable : PackedScene
+@export var hide_on_drag = true
+@export var remove_data_on_failed_drop = false
+@export var remove_if_successfully_dragged = false
+
+signal drag_started(display, data)
 signal drag_ended(display : DieDisplay, data : Dictionary, success : bool)
 var is_dragging : bool = false
 
@@ -14,4 +19,21 @@ func _get_drag_data(_at_position: Vector2) -> Variant:
 	drag_display.data = data
 	set_drag_preview(drag_display)
 	is_dragging = true
+	drag_started.emit(self, data)
 	return data
+
+
+func _on_drag_started(display: Variant, data: Variant) -> void:
+	if hide_on_drag:
+		hide()
+
+
+func _on_drag_ended(display: DieDisplay, data: Dictionary, success: bool) -> void:
+	if success:
+		if remove_if_successfully_dragged:
+			data = {}
+	else:
+		if remove_data_on_failed_drop:
+			data = {}
+		elif hide_on_drag:
+			show()
