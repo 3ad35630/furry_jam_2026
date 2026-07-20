5687706c51dff5b68dd63465e1183e16b7ec4742
Zach Hall <59339962+3ad35630@users.noreply.github.com>
2026-07-20 01:37:52 -0400

    updating the moon rotation script
    - adding delta to the rotations
    - adding the _clock field to the cow display

--- models/scripts/rotation.gd
+++ models/scripts/rotation.gd
@@ -5,7 +5,7 @@ extends Node3D
 @onready var cow1 = $"Soul Cow_001"
 @onready var cow2 = $"Soul Cow_002"
 
-@export var farm_clock: String
+@export var farm_clock: String = "awaken_the_cows"
 @export var fish_speed: float = -0.5
 @export var mill_speed: float = 1.0
 
@@ -13,9 +13,9 @@ func _ready() -> void:
 	Locations.clocks[farm_clock].completed.connect(farm_completed)
 
 func _process(delta: float) -> void:
-	fish.rotation.z = deg_to_rad(rad_to_deg(fish.rotation.z) + fish_speed)
-	mill.rotation.z = deg_to_rad(rad_to_deg(mill.rotation.z) + mill_speed)
+	fish.rotation.z = deg_to_rad(rad_to_deg(fish.rotation.z) + (fish_speed * delta))
+	mill.rotation.z = deg_to_rad(rad_to_deg(mill.rotation.z) + (mill_speed * delta))
 
-func farm_completed() -> void:
+func farm_completed(_clock) -> void:
 	cow1.show()
 	cow2.show()
