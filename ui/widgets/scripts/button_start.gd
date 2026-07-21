f1f5a1bb155bb2e29f9c0c6afe7fcb1b481872e8
Fillan Swift <fillan.swift@gmail.com>
2026-07-20 23:50:42 -0700

    label on guaranteed actions

--- ui/widgets/scripts/button_start.gd
+++ ui/widgets/scripts/button_start.gd
@@ -1,10 +1,11 @@
 extends MainMenuButton
 
 @export var setMode : Enums.GameplayMode = Enums.GameplayMode.MAP
-@export var resumeText : String = 'Resume Game'
+@export var resumeText : String = 'Continue'
 
 func _on_press() -> void:
 	Modes.change_gameplay_mode(setMode)
+
 
 #func _on_mode_changed(mode : Enums.GameplayMode) -> void:
 	#match(mode):
