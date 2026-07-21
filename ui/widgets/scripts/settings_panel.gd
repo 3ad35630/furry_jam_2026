70a7e4af599056492ed97f6b20ac64deb5d82359
Fillan Swift <fillan.swift@gmail.com>
2026-07-21 00:58:42 -0700

    separate controls and settings buttons

--- ui/widgets/scripts/settings_panel.gd
+++ ui/widgets/scripts/settings_panel.gd
@@ -1,18 +1,28 @@
 extends CenterContainer
 
 @onready var credits = %CreditsPanel
-@onready var settings = %SettingsPanel
+@onready var settings = %SettingsPanelMono#%SettingsPanel
+@onready var controls = %ControlsPanel
 
 func _on_button_2_pressed() -> void:
 	visible = true
 	credits.visible = false
 	settings.visible = true
+	controls.visible = false
 	
 func _on_button_3_pressed() -> void:
 	visible = true
 	credits.visible = true
 	settings.visible = false
+	controls.visible = false
 
 
 func _on_back_button_pressed() -> void:
 	visible = false
+
+
+func _on_controls_button_pressed() -> void:
+	visible = true
+	credits.visible = false
+	settings.visible = false
+	controls.visible = true
