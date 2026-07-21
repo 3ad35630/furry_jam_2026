02c33b88adedfab97eddbabf194b4a705b29a613
Zach Hall <59339962+3ad35630@users.noreply.github.com>
2026-07-21 11:14:24 -0400

    fixing bug where continue button was connecting to the same event multiple times

--- ui/widgets/scripts/continue_button.gd
+++ ui/widgets/scripts/continue_button.gd
@@ -3,7 +3,8 @@ extends MainMenuButton
 @export var setMode : Enums.GameplayMode = Enums.GameplayMode.MAP
 
 func _on_ready() -> void:
-	Modes.gameplay_mode_changed.connect(_on_mode_changed)
+	if not Modes.gameplay_mode_changed.is_connected(_on_mode_changed):
+		Modes.gameplay_mode_changed.connect(_on_mode_changed)
 	visible = not Modes.newGame
 
 
@@ -11,6 +12,6 @@ func _on_press() -> void:
 	Modes.change_gameplay_mode(setMode)
 
 
-func _on_mode_changed(mode : Enums.GameplayMode) -> void:
+func _on_mode_changed(_mode : Enums.GameplayMode) -> void:
 	visible = not Modes.newGame
 	Modes.gameplay_mode_changed.disconnect(_on_mode_changed)
