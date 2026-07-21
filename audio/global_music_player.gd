d5b6cd6f3e056e34d3627227d92dc73280a34130
Silverson-EthanG <griffi38@wwu.edu>
2026-07-21 01:24:46 -0700

    Crossfade Audio!

--- audio/global_music_player.gd
+++ audio/global_music_player.gd
@@ -1,4 +1,8 @@
-extends AudioStreamPlayer
+extends Node
+
+@onready var global = $Global_Music
+@onready var location = $Location_Music
+@onready var animation = $AnimationPlayer
 
 @export var world_audio: AudioStream
 
@@ -7,10 +11,19 @@ func _ready() -> void:
 	
 func _location_music(mode : Enums.GameplayMode) -> void:
 	if mode == Enums.GameplayMode.LOCATION and Modes.current_location.audio != null:
-		stop()
-		set_stream(Modes.current_location.audio)
-		play()
+		crossfade_to(Modes.current_location.audio)
 	if mode == Enums.GameplayMode.MAP:
-		stop()
-		set_stream(world_audio)
-		play()
+		crossfade_to(world_audio)
+
+func crossfade_to(audio: AudioStream) -> void:
+	if global.playing and location.playing:
+		return
+		
+	if location.playing:
+		global.stream = audio
+		global.play()
+		animation.play("Location_to_Global")
+	else:
+		location.stream = audio
+		location.play()
+		animation.play("Global_to_Location")
